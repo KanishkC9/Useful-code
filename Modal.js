0d12d6843f824cfcb06216f3280500ccf8c20abe
Kanishk Chaudhary <kanishk@Kanishks-MacBook-Pro.local>
2023-02-12 16:57:30 -0500

    modal

---
+++ Modal.js
@@ -0,0 +1,22 @@
+const styles = {
+    mainModel: {
+      position: 'fixed',
+      zIndex: 10,
+      left: '0',
+      top: '0',
+      width: '100%',
+      height: '100%',
+      padding: 20,
+      overflow: 'auto',
+      backgroundColor: 'rgb(0,0,0)',
+      backgroundColor: 'rgba(0,0,0,0.6)',
+    },
+    modalContent: {
+      margin: isMobile ? '35% auto' : '7% auto',
+      padding: '20',
+      borderRadius: '8px',
+      boxShadow:
+        '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+      alignItems: 'center',
+    },
+  };

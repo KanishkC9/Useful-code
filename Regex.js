0e04544381617c12f03baedd660a1a7b200782c9
Kanishk Chaudhary <kanishk@Kanishks-MacBook-Pro.local>
2023-02-22 10:51:01 -0500

    password regex

--- Regex.js
+++ Regex.js
@@ -1,1 +1,19 @@
 const email =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,12})+$/;
+const passwordErrors = [
+    {
+      error: 'Password must be atleast 8 characters long',
+      regex: /[0-9a-zA-Z!@#$&()\\-`.+,/\)]{8,100}/,
+    },
+    {
+      error: 'Password must include atleast 1 numeric character',
+      regex: /.*[0-9].*/,
+    },
+    {
+      error: 'Password must include atleast 1 uppercase character',
+      regex: /.*[A-Z].*/,
+    },
+    {
+      error: 'Password must include atleast 1 special character',
+      regex: /.*[!@#$%^&*();'[].*/,
+    },
+  ];

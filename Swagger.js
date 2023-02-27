29903a38035c6bdac258fbcbc85f5184ca42d4d6
Kanishk Chaudhary <kanishk@Kanishks-MacBook-Pro.local>
2023-02-27 01:04:37 -0500

    Swagger

---
+++ Swagger.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
